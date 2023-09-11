b2c5226b4dc4158809a7a4afca67977547922e66
root <savvascyp@hotmail.com>
2023-09-11 09:53:23 +0000

    Supabase upload

--- upload.js
+++ upload.js
@@ -5,6 +5,7 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 const filename = 'app.zip'
 
 const file = fs.readFileSync(filename)
-const { data, error } = await supabase.storage.from(process.env.SUPABASE_STORAGE_BUCKET_NAME).upload(filename, file)
-
-console.log({data, error})
+supabase.storage.from(process.env.SUPABASE_STORAGE_BUCKET_NAME).upload(filename, file)
+    .then(() => {
+        console.log("Done");
+    })
